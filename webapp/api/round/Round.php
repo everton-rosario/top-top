9d3fba239925cf6d7c476d043be6bd694fdfedf5
Everton Fernandes Rosario <erosario@gmail.com>
2012-08-31 13:46:33 -0300

    implementacao parcial do create

---
+++ webapp/api/round/Round.php
@@ -0,0 +1,59 @@
+<?php
+if (!defined('__ROOT__')) define('__ROOT__', $_SERVER['DOCUMENT_ROOT'] . '/top-top');
+require_once(__ROOT__.'/lib/config.inc.php');
+
+
+/**
+ * DAO for Round Entity 
+ */
+class Round {
+    private $sdb;
+    private $domain;
+    
+    private $id;
+    private $category;
+    private $user;
+    private $friend;
+    private $items;
+    private $phase;
+
+    public function __construct() {
+        $this->sdb = new SimpleDB(awsAccessKey, awsSecretKey, false);
+        $this->domain = awsRoundDomain;
+        $this->sdb->createDomain($this->domain);
+    }
+
+    public function getSDB() { return $this->sdb; }
+    public function getDomain() { return $this->domain; }
+    
+    public static function create($category, $user, $friend, $items) {
+        $round = new Round();
+        $round->id = uniqid();
+        $round->category = $category;
+        $round->user = $user;
+        $round->friend = $friend;
+        $round->items = $items;
+        $round->phase = 'created';
+        
+        return $round;
+    }
+    
+    public function store() {
+        $request = array();
+        $request['id'] = array('value' => $this->id);
+        $request['category'] = array('value' => $this->category);
+        $request['user'] = array('value' => $this->user);
+        $request['friend'] = array('value' => $this->friend);
+        $request['items'] = array('value' => $this->items);
+        $request['phase'] = array('value' => $this->phase);
+
+        return $this->sdb->putAttributes($this->domain, $this->id, $request);
+        
+    }
+    
+    public function createdResponse() {
+        return array('id' => $this->id,
+                     'items' => $this->items);
+    }
+}    
+?>
--- webapp/api/round/create.php
+++ webapp/api/round/create.php
@@ -2,13 +2,22 @@
 define('__ROOT__', $_SERVER['DOCUMENT_ROOT'] . '/top-top');
 require_once(__ROOT__.'/lib/config.inc.php');
 require_once(__ROOT__.'/api/category/Category.php');
+require_once(__ROOT__.'/api/round/Round.php');
 
 if (!isset($_REQUEST['user']) || !isset($_REQUEST['friend']) || !isset($_REQUEST['category'])) {
     printJSONResult(false, '/api/round/create.php requires (user, friend, category) parameters.');
 
 } else {
+    $user = $_REQUEST['user'];
+    $friend = $_REQUEST['friend'];
+    
     $category = new Category($_REQUEST['category']);
-    printJSONResult(true, 'success', $category->getItems());
+    $items = $category->getItems();
+    
+    $round = Round::create($category->getId(), $user, $friend, $category->getItems());
+    $round->store();
+    
+    printJSONResult(true, 'success', $round->createdResponse());
 }
 
 
