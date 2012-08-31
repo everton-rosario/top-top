bfe324ab6b1cecfc0c7f04dbe3bdda780609b56f
olimpsys <eduardo@olimpsys.com>
2012-08-31 19:32:31 -0300

    fb credits

--- webapp/page/round_result.php
+++ webapp/page/round_result.php
@@ -5,7 +5,7 @@
 
     <div id="round_items_result"></div>
 
-    <div class="game-category-name">
+    <div class="text-center">
         Coins Earned:
         <img src="img/coin.png" width="24" height="24" align="absmiddle" />
         <span id="result_points"></span>
